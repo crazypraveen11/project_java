d39d75e6b21a839da5075f05ffb512911221627a
crazypraveen11 <cspraveensenthilm@gmail.com>
2024-03-31 19:45:28 +0530

    last

--- src/main/java/core_java/arrays/task2d_array.java
+++ src/main/java/core_java/arrays/task2d_array.java
@@ -6,14 +6,15 @@ public class task2d_array
     {
         int [] [] matrix = { {1,2,3}, {4,5,6}, {7,8,9} };
 
-        // for(int row = 0; row < 3; row++)
-        // {
-        //     for(int column = 0; column < 3; column++)
-        //     {
-        //         System.out.print((matrix[row][column] + " ") );
-        //     }
-        // } 
-
+        for(int row = 0; row < 3; row++)
+        {
+            for(int column = 0; column < 3; column++)
+            {
+                System.out.print((matrix[row][column] + " ") );
+            }
+        } 
+        System.out.println();
+        
         for(int values[] : matrix)
         {
             for(int newvalues : values)
@@ -21,6 +22,6 @@ public class task2d_array
                 System.out.print( newvalues + " ");
             }
         }
-
+        
     }
 }
